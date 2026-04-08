022075cb3a5db791b0d8fff462732346d4c84c04
hanxfeng <3556050019@qq.com>
2026-04-08 16:48:36 +0800

    2026/4/8学习记录

--- src/test/java/com/example/javaPractice/JavaPracticeApplicationTests.java
+++ src/test/java/com/example/javaPractice/JavaPracticeApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.javaPractice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 
 @SpringBootTest
 class JavaPracticeApplicationTests {
+	@Autowired
+	private RedisTemplate redisTemplate;
 
 	@Test
 	void contextLoads() {
+		redisTemplate.opsForValue().set("name2","ming");
+		System.out.println(redisTemplate.opsForValue().get("name2"));
 	}
 
 }
